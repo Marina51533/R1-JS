6b4a394de9148a5bbc5029e0e3b25d331664ae5f
Marina51533 <marina51533@gmail.com>
2020-09-28 20:39:16 +0200

    new event

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ let colorTag=document.querySelector('.first');
 colorTag.style.color='red'
 
 let changedTag = document.querySelector('.second');
-
 window.addEventListener('click',changeText);
 function changeText(){
   changedTag.innerHTML='You should buy it!' ;
 }
+
