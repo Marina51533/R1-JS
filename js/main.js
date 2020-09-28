059ea5b6ece2b76abe76404d4fe0502791b8e5b4
Marina51533 <marina51533@gmail.com>
2020-09-28 19:00:47 +0200

    added new rule

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 let colorTag=document.querySelector('.first');
 colorTag.style.color='red'
+
+let changedTag = document.querySelector('.second');
+
+window.addEventListener('click',changeText);
+function changeText(){
+  changedTag.innerHTML='You should buy it!' ;
+}
